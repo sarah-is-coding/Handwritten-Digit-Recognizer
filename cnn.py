8e2f28ddaa58de999bd2ba70e99c7c20264be50a
Sarah Murdock <62572217+sgmdoc7@users.noreply.github.com>
2023-03-23 17:03:45 -0400

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,65 +1,53 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import pandas as pd
-import numpy as np
+import numpy as np  # Linear algebra
+import pandas as pd  # Data processing, CSV file I/O (e.g., pd.read_csv)
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import seaborn as sns
-
-np.random.seed(2)
-
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-
 from sklearn.model_selection import train_test_split
-
-from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.utils.np_utils import to_categorical  # Convert to one-hot-encoding
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Lambda
-from tensorflow.keras.optimizers import Adam
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import RMSprop
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau
-from keras import  backend as K
+from keras import backend as K
 
-from subprocess import check_output
+# Set random seed
+np.random.seed(2)
 
+# Read input data
 train = pd.read_csv("digit-recognizer/train.csv")
 test = pd.read_csv("digit-recognizer/test.csv")
 
-# separate lables and pixel data
-x_train = train.drop(labels = ['label'], axis = 1)
+# Separate labels and pixel data
+x_train = train.drop(labels=['label'], axis=1)
 y_train = train['label']
 
-"""
-Normalization to reduce effects of illumination differences. CNN will work faster. 
-Normalization is reccommended for neural networks over standardization
-"""
+# Normalize the data to reduce effects of illumination differences
+# Normalization is recommended for neural networks over standardization
 x_train = x_train / 255.0
 test = test / 255.0
 
-"""
-Change to a 28 x 28 x 1 3D matrices with 1 being the black-white (greyscale) value. 
-The first parameter (-1) is simply grabbing the number of images in the dataset
-"""
-x_train = x_train.values.reshape(-1,28,28,1)
-test = test.values.reshape(-1,28,28,1)
+# Reshape the data into 28 x 28 x 1 3D matrices
+x_train = x_train.values.reshape(-1, 28, 28, 1)
+test = test.values.reshape(-1, 28, 28, 1)
 
-# one hot encoding
-y_train = to_categorical(y_train, num_classes = 10)
+# One-hot encoding of labels
+y_train = to_categorical(y_train, num_classes=10)
 
+# Split the data into training and validation sets
 seed_value = 30
-x_train, x_temptest, y_train, y_temptest = train_test_split(x_train, y_train, test_size = 0.1, random_state = seed_value)
+x_train, x_temptest, y_train, y_temptest = train_test_split(x_train, y_train, test_size=0.1, random_state=seed_value)
 
-# CNN
-
+# Define the CNN model
 model = Sequential()
-model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', activation ='relu', input_shape = (28,28,1)))
-model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', activation ='relu'))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu'))
 model.add(MaxPooling2D())
 model.add(Dropout(0.25))
-model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', activation ='relu'))
-model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', activation ='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
 model.add(MaxPooling2D())
 model.add(Dropout(0.25))
 model.add(Flatten())
@@ -67,12 +55,13 @@ model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.25))
 model.add(Dense(10, activation='softmax'))
 
-#define optimizer
-optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+# Define the optimizer
+optimizer = RMSprop(learning_rate=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
-#compile model
-model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+# Compile the model
+model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
 
+# Define the image data generator
 datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
