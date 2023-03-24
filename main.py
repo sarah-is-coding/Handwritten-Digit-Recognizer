55f21e8814f49374a2b8721425249eb73da563ea
sgmdoc7 <sg.mdoc7@gmail.com>
2023-03-24 14:41:49 -0400

    real time digit recognition

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.uix.widget import Widget
 from kivy.properties import ColorProperty
 import numpy as np
 import tensorflow as tf
+from kivy.clock import Clock
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Load the pretrained model
 model = tf.keras.models.load_model('mnist_digit_model')
@@ -16,6 +19,7 @@ class GridCell(Widget):
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             self.color = App.get_running_app().pencil_color
+            App.get_running_app().schedule_update_prediction()
             return True
         return super().on_touch_down(touch)
 
@@ -23,13 +27,10 @@ class GridCell(Widget):
     def on_touch_move(self, touch):
         if self.collide_point(*touch.pos):
             self.color = App.get_running_app().pencil_color
+            App.get_running_app().schedule_update_prediction()
             return True
         return super().on_touch_move(touch)
 
-# Define the custom widget Buttons
-class Buttons(Widget):
-    def press():
-        print('hello')
 
 # Define the Kivy language string for UI elements
 KV = '''
@@ -68,26 +69,34 @@ BoxLayout:
     BoxLayout:
         orientation: 'horizontal'
         size_hint_y: None
-        height: sp(50)
+        height: sp(60)
         spacing: sp(10)
         Button:
             text: 'Clear'
-            background_color: 0.5, 0.5, 0.5, 1
+            font_size: sp(24)
+            background_color: 0, 0, 0, 0
             background_normal: ''
+            color: 1, 1, 1, 1
+            size_hint_x: 0.5
+            bold: True
             on_release: [setattr(x, 'color', '#ffffffff') for x in grid.children]
             on_release: app.reset_prediction()
+            canvas.before:
+                Color:
+                    rgba: 0.2, 0.6, 0.8, 1
+                RoundedRectangle:
+                    pos: self.pos
+                    size: self.size
+                    radius: [sp(10),]
 
         Label:
             id: prediction
             text: 'Prediction: _'
-            font_size: sp(20)
+            font_size: sp(24)
             color: 0.2, 0.2, 0.2, 1
+            bold: True
+            size_hint_x: 0.5
 
-        Button:
-            text: 'Submit'
-            background_color: 0.2, 0.6, 0.8, 1
-            background_normal: ''
-            on_release: app.get_prediction(grid.children)
 '''
 
 # Define the main app class
@@ -101,7 +110,8 @@ class PixelGridApp(App):
         self.root.ids.prediction.text = 'Prediction: _'
 
     # Get the prediction based on the drawn digit
-    def get_prediction(self, grid_children):
+    def update_prediction(self, *args):
+        grid_children = self.root.ids.grid.children
         temp_digit_arr = [abs(int(x.color[0]) - 1) for x in grid_children]
         digit_arr = np.append(temp_digit_arr, np.array(temp_digit_arr), axis=0)
         digit_arr = np.flipud(digit_arr)
@@ -109,6 +119,10 @@ class PixelGridApp(App):
 
         model_prediction = np.argmax(model.predict(digit_arr))
         self.root.ids.prediction.text = f'Prediction: {model_prediction}'
+
+    def schedule_update_prediction(self, *args):
+        Clock.unschedule(self.update_prediction)
+        Clock.schedule_once(self.update_prediction, 0.1)
 
     # Build the app UI
     def build(self):
